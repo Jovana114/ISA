56714423be9b652fd3ac4d41df1d9403f313b7be
Filip Mrdjanovic <mrkifilip@gmail.com>
2022-11-10 16:13:05 +0100

    JWT fix, added token in response

--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.ERole;
+import com.example.demo.models.Role;
+
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
+  Optional<Role> findByName(ERole name);
 }
