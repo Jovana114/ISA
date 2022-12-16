c75ad669d65f3ac68c1db01b05228defcdd4c7a0
Filip Mrdjanovic <filipmrdjanovic@gmail.com>
2022-12-16 15:21:38 +0100

    Added sorting for double table

--- frontend/src/components/Table/DoubleTableCenterAppointment.tsx
+++ frontend/src/components/Table/DoubleTableCenterAppointment.tsx
@@ -24,7 +24,137 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
-import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { Checkbox, FormControlLabel, FormGroup, TableSortLabel } from "@mui/material";
+
+import { visuallyHidden } from "@mui/utils";
+
+
+
+interface Data {
+  id: number,
+  name: string,
+  address: string,
+  averageRating: number,
+  appointments: any
+}
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort<T>(
+  array: any,
+  comparator: (a: T, b: T) => number
+) {
+  const stabilizedThis = array.map((el: any, index: any) => [el, index] as [T, number]);
+  stabilizedThis.sort((a: any, b: any) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el: any) => el[0]);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Data;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: true,
+    label: "Name",
+  },
+  {
+    id: "address",
+    numeric: false,
+    disablePadding: false,
+    label: "Address",
+  },
+  {
+    id: "averageRating",
+    numeric: false,
+    disablePadding: false,
+    label: "Rating",
+  },
+];
+
+interface EnhancedTableProps {
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, onRequestSort } = props;
+
+  const createSortHandler = (property: keyof Data) => (
+    event: React.MouseEvent<unknown>
+  ) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableRow>
+      <TableCell></TableCell>
+      {headCells.map((headCell) => (
+        <TableCell
+          key={headCell.id}
+          align={headCell.numeric ? "right" : "center"}
+          padding={headCell.disablePadding ? "none" : "normal"}
+          sortDirection={orderBy === headCell.id ? order : false}
+        >
+          <TableSortLabel
+            active={orderBy === headCell.id}
+            direction={orderBy === headCell.id ? order : "asc"}
+            onClick={createSortHandler(headCell.id)}
+          >
+            {headCell.label}
+            {orderBy === headCell.id ? (
+              <Box component="span" sx={visuallyHidden}>
+                {order === "desc" ? "sorted descending" : "sorted ascending"}
+              </Box>
+            ) : null}
+          </TableSortLabel>
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
+
+
 
 function CenterAppointment(
   id: number,
@@ -74,7 +204,7 @@ function Row(props: { row: ReturnType<typeof CenterAppointment> }) {
           </TableCell>
           <TableCell>{row.name}</TableCell>
           <TableCell>{row.address}</TableCell>
-          <TableCell>{row.averageRating}</TableCell>
+          <TableCell align="center">{row.averageRating}</TableCell>
         </TableRow>
         <TableRow>
           <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -150,6 +280,10 @@ export default function DoubleTableCenterAppointment() {
     },
   };
 
+  // ORDER
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] = React.useState<keyof Data>("name");
+
   const [centerRows, setCenterRows] = useState([]);
   const [appointmentRows, setAppointmentRows] = useState([]);
   const [searchText, setSearchText] = useState("");
@@ -169,6 +303,17 @@ export default function DoubleTableCenterAppointment() {
   const STEP = 1;
   const MIN = 0;
   const MAX = 10;
+
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(event.target.checked);
@@ -506,11 +651,19 @@ export default function DoubleTableCenterAppointment() {
                 </Collapse>
               </TableCell>
             </TableRow>
+
+            <EnhancedTableHead
+                  order={order}
+                  orderBy={orderBy}
+                  onRequestSort={handleRequestSort}
+                  rowCount={rows.length}
+                />
           </TableHead>
           <TableBody>
-            {rows.map((row: any) => (
-              <Row key={row.name + row.id} row={row} />
-            ))}
+            {stableSort(rows, getComparator(order, orderBy))
+              .map((row: any) => {
+                return (<Row key={row.name + row.id} row={row} />)
+            })}
           </TableBody>
         </Table>
       </TableContainer>
