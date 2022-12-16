21d0c5fcb5dc89d0d9c431cb60815764f79cb82a
unknown <81173605+Jovana114@users.noreply.github.com>
2022-12-16 15:59:25 +0100

    front update tables

--- frontend/src/components/RegistertedUsers/RegistertedUsers.tsx
+++ frontend/src/components/RegistertedUsers/RegistertedUsers.tsx
@@ -10,15 +10,142 @@ import {
 } from "@mui/material";
 import axios from "axios";
 import Dialog from "@mui/material/Dialog";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Box from "@mui/material/Box";
+import { visuallyHidden } from "@mui/utils";
 
 interface RegistertedUsersProps {
   open: boolean;
   onClose: () => void;
 }
 
+interface Data {
+  firstname: string;
+  lastname: string;
+  date_when_blood_was_donated: string;
+}
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort<T>(
+  array: readonly T[],
+  comparator: (a: T, b: T) => number
+) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Data;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "firstname",
+    numeric: false,
+    disablePadding: true,
+    label: "Firstname",
+  },
+  {
+    id: "lastname",
+    numeric: false,
+    disablePadding: false,
+    label: "Lastname",
+  },
+  {
+    id: "date_when_blood_was_donated",
+    numeric: true,
+    disablePadding: false,
+    label: "Date when blood was donated",
+  },
+];
+
+interface EnhancedTableProps {
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, onRequestSort } = props;
+
+  const createSortHandler = (property: keyof Data) => (
+    event: React.MouseEvent<unknown>
+  ) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableRow>
+      {headCells.map((headCell) => (
+        <TableCell
+          key={headCell.id}
+          align={headCell.numeric ? "right" : "left"}
+          padding={headCell.disablePadding ? "none" : "normal"}
+          sortDirection={orderBy === headCell.id ? order : false}
+        >
+          <TableSortLabel
+            active={orderBy === headCell.id}
+            direction={orderBy === headCell.id ? order : "asc"}
+            onClick={createSortHandler(headCell.id)}
+          >
+            {headCell.label}
+            {orderBy === headCell.id ? (
+              <Box component="span" sx={visuallyHidden}>
+                {order === "desc" ? "sorted descending" : "sorted ascending"}
+              </Box>
+            ) : null}
+          </TableSortLabel>
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
+
 export default function RegistertedUsers({ open, onClose }: RegistertedUsersProps) {
 
     const [data, setData] = useState([]);
+    const [order, setOrder] = React.useState<Order>("asc");
+    const [orderBy, setOrderBy] = React.useState<keyof Data>("firstname");
+    const [rows, setRows] = useState([]);
   
     const config = {
       headers: {
@@ -29,7 +156,16 @@ export default function RegistertedUsers({ open, onClose }: RegistertedUsersProp
 
     const handleClose = () => {
         onClose();
-      };
+    };
+
+    const handleRequestSort = (
+      event: React.MouseEvent<unknown>,
+      property: keyof Data
+    ) => {
+      const isAsc = orderBy === property && order === "asc";
+      setOrder(isAsc ? "desc" : "asc");
+      setOrderBy(property);
+    };
   
     useEffect(() => {
       (async () => {
@@ -63,9 +199,9 @@ export default function RegistertedUsers({ open, onClose }: RegistertedUsersProp
                   <TableCell>Gender</TableCell>
                   <TableCell>Date of donation</TableCell>
                 </TableRow>
-             </TableHead>
+                </TableHead>
               <TableBody>
-                {
+              {
                 data.map((row: any, _key) => (
                   <TableRow
                     key={_key}
@@ -81,7 +217,8 @@ export default function RegistertedUsers({ open, onClose }: RegistertedUsersProp
                     <TableCell>{row.gender}</TableCell>
                     <TableCell>{row.date_when_blood_was_donated}</TableCell>
                   </TableRow>
-                ))}
+                )
+              )};
               </TableBody>
             </Table>
           </TableContainer>
